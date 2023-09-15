9f24e6942614ee938050ce24647b4b53a7f5f597
Bruno Marcon <55155016+Bruno-Marcon@users.noreply.github.com>
2023-09-15 10:43:42 -0300

    Create-HomePage

--- setup/settings.py
+++ setup/settings.py
@@ -115,10 +115,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
